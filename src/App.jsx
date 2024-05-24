7ad699e764f13feba7df8dd884aef04bec30345d
Juan Ignacio Caprioli <juanicaprioli16@outlook.com>
2024-05-24 19:55:38 -0300

    Completed project, with possible minimal modifications in the future

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import './App.module.css'
+import './App.module.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {ItemListContainer} from "./components/ItemListContainer/ItemListContainer.jsx";
 import {Cart} from "./components/Cart/Cart.jsx";
